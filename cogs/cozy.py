50b7b5e3e26e81b6e2506477aa5510e430097c54
ChattyRS <rschatty@gmail.com>
2021-06-03 22:35:39 +0200

    Add theatre of blood hard mode

--- cogs/cozy.py
+++ cogs/cozy.py
@@ -37,7 +37,7 @@ wom_metrics = ["overall", "attack", "defence", "strength", "hitpoints", "ranged"
                "chaos_elemental", "chaos_fanatic", "commander_zilyana", "corporeal_beast", "crazy_archaeologist", "dagannoth_prime", "dagannoth_rex", "dagannoth_supreme",
                "deranged_archaeologist", "general_graardor", "giant_mole", "grotesque_guardians", "hespori", "kalphite_queen", "king_black_dragon", "kraken", "kreearra", 
                "kril_tsutsaroth", "mimic", "nightmare", "obor", "sarachnis", "scorpia", "skotizo", "tempoross", "the_gauntlet", "the_corrupted_gauntlet", "theatre_of_blood",
-               "thermonuclear_smoke_devil", "tzkal_zuk", "tztok_jad", "venenatis", "vetion", "vorkath", "wintertodt", "zalcano", "zulrah", "ehp", "ehb"]
+               "theatre_of_blood_hard_mode", "thermonuclear_smoke_devil", "tzkal_zuk", "tztok_jad", "venenatis", "vetion", "vorkath", "wintertodt", "zalcano", "zulrah", "ehp", "ehb"]
 
 class Cozy(commands.Cog):
     def __init__(self, bot):
