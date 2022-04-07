cd8524d06e039063dcfb99047441b15f275c1fcd
ChattyRS <rschatty@gmail.com>
2022-04-07 23:49:45 +0200

    Fix gains command XP formatting for +10 or more levels gained

--- cogs/runescape.py
+++ cogs/runescape.py
@@ -1309,7 +1309,7 @@ class Runescape(commands.Cog):
                 raise commands.CommandError(message=f'Could not find xp gains for: `{name}`.')
             data = await r.text()
 
-        for i in range(1,100):
+        for i in range(100, 1, -1):
             data = data.replace(f'+{i}', '')
 
         try:
