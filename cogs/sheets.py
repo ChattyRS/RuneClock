6563301f6e791255099a8b13a34e15bcb93d4dce
Chatty <rschatty@gmail.com>
2024-11-12 23:12:46 +0100

    Add page navigation to sheets command

--- cogs/sheets.py
+++ cogs/sheets.py
@@ -1,9 +1,147 @@
 import math
+import traceback
+from typing import Any
 import discord
 from discord.ext import commands
 from discord.ext.commands import Cog
 from gspread_asyncio import AsyncioGspreadClient, AsyncioGspreadSpreadsheet, AsyncioGspreadWorksheet
 from src.bot import Bot
+
+class SheetPageView(discord.ui.View):
+    def __init__(self, bot: Bot) -> None:
+        super().__init__(timeout=None)
+        self.bot: Bot = bot
+    
+    @discord.ui.button(label='Previous', style=discord.ButtonStyle.blurple, custom_id='display_sheet_previous_page_button')
+    async def previous(self, interaction: discord.Interaction, _: discord.ui.Button) -> None:
+        # Validate permissions
+        if not interaction.message:
+            await interaction.response.send_message('Could not find interaction message.', ephemeral=True)
+            return
+        # Get message and validate action
+        embed: discord.Embed = interaction.message.embeds[0]
+        if not embed.footer or not embed.footer.text:
+            await interaction.response.send_message('Embed footer was empty.', ephemeral=True)
+            return
+        footer_parts: list[str] = embed.footer.text.split('/')[0:6]
+        value_columns, num_rows, total_rows, current_page, num_pages, key = footer_parts
+        sheet_name: str = embed.footer.text.split(f'{key}/')[1]
+        value_columns = int(value_columns)
+        num_rows = int(num_rows)
+        total_rows = int(total_rows)
+        current_page = int(current_page)
+        num_pages = int(num_pages)
+        if current_page <= 1:
+            await interaction.response.send_message('There is no previous page to go back to. You are already on the first page.', ephemeral=True)
+            return
+        target_page: int = current_page - 1
+        page_rows: int = num_rows if target_page < num_pages else (total_rows % num_rows)
+
+        await interaction.response.defer()
+
+        embed.remove_footer()
+        embed.set_footer(text=f'{value_columns}/{num_rows}/{total_rows}/{target_page}/{num_pages}/{key}/{sheet_name}')
+
+        embed.description = f'Page {target_page} / {num_pages}'
+
+        agc: AsyncioGspreadClient = await self.bot.agcm.authorize()
+        ss: AsyncioGspreadSpreadsheet = await agc.open_by_key(key)
+
+        sheet: AsyncioGspreadWorksheet = await ss.worksheet(sheet_name)
+        values: list[list[str]] = await sheet.get_all_values()
+
+        row_start: int = (target_page - 1) * num_rows
+        row_end: int = row_start + page_rows
+        if not values or len(values) <= 1 or len(values) - 1 < row_end:
+            await interaction.response.send_message(f'Could not fetch requested data from sheet (rows {row_start + 2}-{row_end + 2}). Please verify that it is still there.', ephemeral=True)
+            return
+        
+        header: list[str] = values[0]
+        if len(header) <= 1:
+            raise commands.CommandError(message=f'Sheet `{sheet_name}` must contain at least 2 columns to be able to display any data.')
+        header = header[:value_columns+1] if len(header) > value_columns + 1 else header
+
+        identity_column_name: str = header[0]
+        value_column_names: list[str] = header[1:]
+        value_columns = len(value_column_names)
+
+        data: list[list[str]] = [row[:1+value_columns+1] for row in values[row_start+1:row_end+1]]
+
+        embed.clear_fields()
+        for row in data:
+            embed.add_field(name=identity_column_name, value=f'**{row[0]}**', inline=False)
+            for i, column in enumerate(row[1:]):
+                embed.add_field(name=value_column_names[i], value=column, inline=True)
+
+        await interaction.message.edit(embed=embed, view=self)
+
+    @discord.ui.button(label='Next', style=discord.ButtonStyle.blurple, custom_id='display_sheet_next_page_button')
+    async def next(self, interaction: discord.Interaction, _: discord.ui.Button) -> None:
+        # Validate permissions
+        if not interaction.message:
+            await interaction.response.send_message('Could not find interaction message.', ephemeral=True)
+            return
+        # Get message and validate action
+        embed: discord.Embed = interaction.message.embeds[0]
+        if not embed.footer or not embed.footer.text:
+            await interaction.response.send_message('Embed footer was empty.', ephemeral=True)
+            return
+        footer_parts: list[str] = embed.footer.text.split('/')[0:6]
+        value_columns, num_rows, total_rows, current_page, num_pages, key = footer_parts
+        sheet_name: str = embed.footer.text.split(f'{key}/')[1]
+        value_columns = int(value_columns)
+        num_rows = int(num_rows)
+        total_rows = int(total_rows)
+        current_page = int(current_page)
+        num_pages = int(num_pages)
+        if current_page >= num_pages:
+            await interaction.response.send_message('There is no next page to go to. You are already on the last page.', ephemeral=True)
+            return
+        target_page: int = current_page + 1
+        page_rows: int = num_rows if target_page < num_pages else (total_rows % num_rows)
+
+        await interaction.response.defer()
+
+        embed.remove_footer()
+        embed.set_footer(text=f'{value_columns}/{num_rows}/{total_rows}/{target_page}/{num_pages}/{key}/{sheet_name}')
+
+        embed.description = f'Page {target_page} / {num_pages}'
+
+        agc: AsyncioGspreadClient = await self.bot.agcm.authorize()
+        ss: AsyncioGspreadSpreadsheet = await agc.open_by_key(key)
+
+        sheet: AsyncioGspreadWorksheet = await ss.worksheet(sheet_name)
+        values: list[list[str]] = await sheet.get_all_values()
+
+        row_start: int = (target_page - 1) * num_rows
+        row_end: int = row_start + page_rows
+        if not values or len(values) <= 1 or len(values) - 1 < row_end:
+            await interaction.response.send_message(f'Could not fetch requested data from sheet (rows {row_start + 2}-{row_end + 2}). Please verify that it is still there.', ephemeral=True)
+            return
+        
+        header: list[str] = values[0]
+        if len(header) <= 1:
+            raise commands.CommandError(message=f'Sheet `{sheet_name}` must contain at least 2 columns to be able to display any data.')
+        header = header[:value_columns+1] if len(header) > value_columns + 1 else header
+
+        identity_column_name: str = header[0]
+        value_column_names: list[str] = header[1:]
+        value_columns = len(value_column_names)
+
+        data: list[list[str]] = [row[:1+value_columns+1] for row in values[row_start+1:row_end+1]]
+
+        embed.clear_fields()
+        for row in data:
+            embed.add_field(name=identity_column_name, value=f'**{row[0]}**', inline=False)
+            for i, column in enumerate(row[1:]):
+                embed.add_field(name=value_column_names[i], value=column, inline=True)
+
+        await interaction.message.edit(embed=embed, view=self)
+    
+    async def on_error(self, interaction: discord.Interaction, error: Exception, item: discord.ui.Item[Any]) -> None:
+        await interaction.response.send_message(error, ephemeral=True)
+        print(error)
+        traceback.print_tb(error.__traceback__)
 
 class Sheets(Cog):
     def __init__(self, bot: Bot) -> None:
@@ -18,10 +156,6 @@ class Sheets(Cog):
             key (str | None): The key to your sheet. This is the part of the URL that comes after "spreadsheets/d/" and before "/edit".
             sheet_name (str | None, optional): The sheet name to display. Defaults to None for the first sheet.
             value_columns (int | None, optional): The number of value columns to display. Defaults to None for auto-detect. Max number of value columns supported is 4, excluding the first column that is used as row title / name.
-
-        Raises:
-            commands.CommandError: _description_
-            commands.CommandError: _description_
         '''
         self.bot.increment_command_counter()
         await ctx.channel.typing()
@@ -61,13 +195,17 @@ class Sheets(Cog):
 
         data: list[list[str]] = [row[:1+value_columns+1] for row in values[1:rows+1]]
 
-        embed = discord.Embed(title=f'**{sheet_name}**', colour=0x00b2ff)
+        embed = discord.Embed(title=f'**{sheet_name}**', description=f'Page 1 / {math.ceil((len(values)-1)/rows)}', colour=0x00b2ff)
         for row in data:
             embed.add_field(name=identity_column_name, value=f'**{row[0]}**', inline=False)
             for i, column in enumerate(row[1:]):
                 embed.add_field(name=value_column_names[i], value=column, inline=True)
 
-        await ctx.send(embed=embed)
+        # Footer encodes: number of value columns, number of rows per page, total number of rows, current page, number of pages, key, sheet name
+        embed.set_footer(text=f'{value_columns}/{rows}/{len(values)-1}/1/{math.ceil((len(values)-1)/rows)}/{key}/{sheet_name}')
+
+        view = SheetPageView(self.bot)
+        await ctx.send(embed=embed, view=view)
 
 
 async def setup(bot: Bot) -> None:
